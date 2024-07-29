8a4830946d1b32e174c25cb5ec77c6e339c7f479
Michael Chop <007micle007@gmail.com>
2024-07-29 21:48:00 +0300

    Implemented the basic chat page functionality

--- store/index.js
+++ store/index.js
@@ -1,9 +1,17 @@
 export const state = () => ({
-
+  user: {},
+  messages: []
 })
 
-export const actions = {
-  SOCKET_newMessage(ctx, data) {
-    console.log('SOCKET_newMessage', data)
+export const mutations = {
+  setUser(state, user) {
+    state.user = user
+  },
+  clearData(state) {
+    state.user = {}
+    state.messages = []
+  },
+  SOCKET_newMessage(state, message) {
+    state.messages.push(message)
   }
 }
